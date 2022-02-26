fe03a39f07b105073968c7b80d53734ab609ee9f
SigmaTNT <100475104+SigmaTNT@users.noreply.github.com>
2022-02-26 18:28:24 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,108 @@
+//#include<iostream>
+//#include<fstream>
+//
+//using namespace std;
+//
+//void fileread(ifstream& fin)
+//{
+//	fin.open("input.txt", ios::in);
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	ifstream fin;
+//	fileread(fin);
+//
+//	int row;
+//	
+//	int arr[3][3];
+//
+//	
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		for (int j = 0; j < 3; j++)
+//		{
+//			fin >> arr[i][j];
+//
+//
+//		}
+//
+//	}
+//
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cout << arr[i][i] << " ";
+//	}
+//
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//
+//}
+
+
+
+//#include<iostream>
+//#include<fstream>
+//
+//using namespace std;
+//
+//void fileread(ifstream& fin)
+//{
+//	fin.open("input.txt", ios::in);
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	ifstream fin;
+//	fileread(fin);
+//
+//	int row;
+//
+//	int arr[3][3];
+//
+//
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		for (int j = 0; j < 3; j++)
+//		{
+//			fin >> arr[i][j];
+//
+//
+//		}
+//
+//	}
+//
+//
+//	for (int i = 0,k=2; i < 3; i++,k--)
+//	{
+//		cout << arr[i][k] << " ";
+//	}
+//
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//
+//}
+
+
+
+
+
+
+
